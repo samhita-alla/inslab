8db9ead7240965933b7ac936e0671b457edf9e02
sekharkaredla <sekhar.karedla@gmail.com>
2018-01-09 20:39:39 +0530

    3des

---
+++ des/triple_des.py
@@ -0,0 +1,11 @@
+from pyDes import *
+
+data = "sekhar karedla"
+
+e = triple_des("ABCDEFGHHGFEDCBA", CBC, "\0\0\0\0\0\0\0\0", pad=None, padmode=PAD_PKCS5)
+
+encrypted_data = e.encrypt(data)
+
+print "encrypted data is : "+encrypted_data
+
+print "decrypted data is : "+e.decrypt(encrypted_data)
