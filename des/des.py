609bf27c154e9e53defc2fbf9b879daa366b41fc
Dasarada Ram Reddy <dachi8958@gmail.com>
2018-01-10 14:17:49 +0530

    plus space

--- des/des.py
+++ des/des.py
@@ -6,6 +6,6 @@ e = des("DESCRYPT", CBC, "\0\0\0\0\0\0\0\0", pad=None, padmode=PAD_PKCS5)
 
 encrypted_data = e.encrypt(data)
 
-print "encrypted data is : "+encrypted_data
+print "encrypted data is : " + encrypted_data
 
-print "decrypted data is : "+e.decrypt(encrypted_data)
+print "decrypted data is : " + e.decrypt(encrypted_data)
